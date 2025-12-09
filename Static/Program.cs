c9aaa14e2039c0a3996b655a41a38f69ddfc4d30
Alfonce Yano <ogongoyanojnr@gmail.com>
2025-12-09 12:49:42 +0300

    🍵🍵🍵 partial classes

--- Static/Program.cs
+++ Static/Program.cs
@@ -88,108 +88,145 @@
 //****************************************INTERFACES ***************************************
 
 
-public interface IEnemy { 
+//public interface IEnemy { 
 
-void Attack();
-void Defend();
+//void Attack();
+//void Defend();
 
-}
+//}
 
-public interface IWild
+//public interface IWild
+//{
+//    void habitat();
+//}
+
+//// using interfaces to create abstractions
+//public interface ITamedEnemy : IEnemy {
+//    void Feed();
+//    void Calm();
+
+
+//}
+
+
+//// implementing the interface
+
+//class Dragon : IEnemy 
+//{
+//    public void Attack() { Console.WriteLine(" Dragon Breathes fire"); }
+//    public void Defend() { Console.WriteLine("Dragon Spreads wings"); }
+//}
+
+//class Vampire : IEnemy
+//{
+//    public void Attack()
+//    {
+//        Console.WriteLine("Vampire realeases its fangs");
+//    }
+
+//    public void Defend()
+//    {
+//        Console.WriteLine("Vampire dissapears in the air");
+//    }
+//}
+
+//public class Dog : ITamedEnemy
+//{
+//    public void Attack()
+//    {
+//        throw new NotImplementedException();
+//    }
+
+//    public void Calm()
+//    {
+//        throw new NotImplementedException();
+//    }
+
+//    public void Defend()
+//    {
+//        throw new NotImplementedException();
+//    }
+
+//    public void Feed()
+//    {
+//        throw new NotImplementedException();
+//    }
+//}
+
+//// using interface for multiple inheritance
+
+//public class Lion : IEnemy, IWild
+//{
+//    public void Attack()
+//    {
+//        throw new NotImplementedException();
+//    }
+
+//    public void Defend()
+//    {
+//        throw new NotImplementedException();
+//    }
+
+//    public void habitat()
+//    {
+//        throw new NotImplementedException();
+//    }
+//}
+//public class Program
+//{
+//    private static void Main (string[] args)
+//    {
+//        IEnemy e1 = new Dragon();
+//        IEnemy e2 = new Vampire();
+//        //e1.Attack();
+//        //e2.Attack();
+//        //e1.Defend();
+//        //e2.Defend();
+
+//        List<IEnemy> list = new List<IEnemy>();
+//        list.Add(e1);
+//        list.Add(e2);
+
+//        foreach (IEnemy e in list) { 
+//        e.Attack();
+//            e.Defend();
+//        }
+//    }
+//}
+
+
+//****************************************PARTIAL CLASSES***************************************
+// can be used to divide a class into different components
+
+public partial class Book// defining attributes
 {
-    void habitat();
+    public string title { get; set; }
+    public string description { get; set; }
+    public string author { get; set; }
 }
 
-// using interfaces to create abstractions
-public interface ITamedEnemy : IEnemy {
-    void Feed();
-    void Calm();
-
-
-}
-
-
-// implementing the interface
-
-class Dragon : IEnemy 
+public partial class Book // defining methods
 {
-    public void Attack() { Console.WriteLine(" Dragon Breathes fire"); }
-    public void Defend() { Console.WriteLine("Dragon Spreads wings"); }
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Title: {title}");
+        Console.WriteLine($"Description: {description}");
+        Console.WriteLine($"Author: {author}");
+    }
+
 }
 
-class Vampire : IEnemy
-{
-    public void Attack()
-    {
-        Console.WriteLine("Vampire realeases its fangs");
-    }
 
-    public void Defend()
-    {
-        Console.WriteLine("Vampire dissapears in the air");
-    }
-}
-
-public class Dog : ITamedEnemy
-{
-    public void Attack()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Calm()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Defend()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Feed()
-    {
-        throw new NotImplementedException();
-    }
-}
-
-// using interface for multiple inheritance
-
-public class Lion : IEnemy, IWild
-{
-    public void Attack()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Defend()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void habitat()
-    {
-        throw new NotImplementedException();
-    }
-}
 public class Program
 {
-    private static void Main (string[] args)
+    public static void Main( string[] args)
     {
-        IEnemy e1 = new Dragon();
-        IEnemy e2 = new Vampire();
-        //e1.Attack();
-        //e2.Attack();
-        //e1.Defend();
-        //e2.Defend();
+        Book book1 = new Book();
+        book1.title = "Art of Programing";
+        book1.description = "A Beginners Programing Guide";
+        book1.author = "Linus Tovads";
 
-        List<IEnemy> list = new List<IEnemy>();
-        list.Add(e1);
-        list.Add(e2);
+        book1.ShowInfo();
 
-        foreach (IEnemy e in list) { 
-        e.Attack();
-            e.Defend();
-        }
     }
 }
